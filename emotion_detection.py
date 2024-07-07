cb1a359e835b9f01eba080c4b69a4361912702c2
Developercop <100005394+Developercop@users.noreply.github.com>
2024-07-07 18:40:27 -0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,1 +1,3 @@
+'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 { "raw_document": { "text": emotion_detector } }
